271098a772b5b5d773f7d5f7bb1a9380b02fcab5
betoramiz <maramirez10@gmail.com>
2014-12-17 15:13:09 -0700

    primer commit

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class Empleado
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+    }
+
+    public class ValuesController : ApiController
+    {
+        List<Empleado> listaEmpleado = null;
+        public ValuesController()
+        {
+            listaEmpleado = new List<Empleado>();
+            listaEmpleado.Add(new Empleado() { id = 1, Name = "Alberto", Department="Departen 1"});
+            listaEmpleado.Add(new Empleado() { id = 2, Name = "Alejandro", Department = "Departen 1" });
+            listaEmpleado.Add(new Empleado() { id = 3, Name = "Kitzia", Department = "Departen 2" });
+            listaEmpleado.Add(new Empleado() { id = 4, Name = "Kenia", Department = "Departen 1" });
+            listaEmpleado.Add(new Empleado() { id = 5, Name = "Bernardo", Department = "Departen 2" });
+        }
+
+        [ActionName("Empleados")]
+        [HttpGet]
+        public IEnumerable<Empleado> Empleados()
+        {
+            return listaEmpleado;
+        }
+
+        // GET api/values/5
+        public Empleado Get(int id)
+        {
+            var empleado = listaEmpleado.Where(i => i.id == id).SingleOrDefault();
+            return empleado;
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ consumingURL/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using System.Xml.Linq;
+using System.Xml;
+using System.Text.RegularExpressions;
+
+namespace consumingURL
+{
+    class Program
+    {
+        public static string RemoveNS(string doc)
+        {
+            return Regex.Replace(doc, @"xmlns[:xsi|:xsd]*="".*?""", String.Empty);
+        }
+
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:19735/api/Values/Empleados";
+
+            var client = new WebClient();
+            client.Headers.Add("Accept", "application/xml");
+            client.Headers.Add("Content-Type", "application/xml");            
+            var result = client.DownloadString(url);
+            result = RemoveNS(result);
+
+            XDocument xdoc = XDocument.Parse(result);
+            var numeroEmpleados = (from e in xdoc.Descendants("Empleado")
+                                   select e).Count();
+
+            var empleadoId = xdoc.Descendants("Empleado").Where(c => (int)c.Element("id") == 2);
+
+            Console.WriteLine(numeroEmpleados.ToString());
+            Console.WriteLine(empleadoId.Elements("Name").FirstOrDefault().Value);
+
+            Console.ReadLine();
+        }
+    }
+}
